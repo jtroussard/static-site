96693628f97d0eeb5126a4c45124058f2fe9810a
Troussard, Jacques <Jacques.Troussard@McKesson.com>
2021-03-16 20:28:21 -0400

    adds a partial implementation of the portfolios section using the data file

--- personal_website2/main/routes.py
+++ personal_website2/main/routes.py
@@ -13,7 +13,8 @@ with open(data_filename) as file:
 def home():
     if data:
         resume = get_resume_file(data)
-    return render_template("home.html", data=data, resume=resume)
+        portfolio_data = build_distinct_portfolio_type_data(data)
+    return render_template("home.html", data=data, resume=resume, portfolio_data=portfolio_data)
 
 # Helpers
 def get_resume_file(data):
@@ -22,4 +23,21 @@ def get_resume_file(data):
             print(f"{file['filename']}.{file['extension']}")
             return f"{file['filename']}.{file['extension']}"
     return "#"
+
+def build_distinct_portfolio_type_data(data):
+    distinct_types_list = []
+    distinct_types = []
+    for portfolio in data['portfolios']:
+        if portfolio['project_type'] not in distinct_types:
+            distinct_type_data = {
+                "project_type": "",
+                "project_img": "",
+                "project_color": ""
+            }
+            distinct_type_data['project_type'] = portfolio['project_type']
+            distinct_type_data['project_img'] = portfolio['project_img']
+            distinct_type_data['project_color'] = portfolio['project_color']
+            distinct_types_list.append(distinct_type_data)
+            distinct_types.append(portfolio['project_type'])
+    return distinct_types_list
 
