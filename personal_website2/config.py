d19d49cf334ebad01cf7c348ea4ab6834ec5aeba
Troussard, Jacques <Jacques.Troussard@McKesson.com>
2021-03-22 18:46:56 -0400

    adds windows compatibility for config/run.py

--- personal_website2/config.py
+++ personal_website2/config.py
@@ -1,10 +1,21 @@
 import os
 import json
+import platform
 
-with open('/etc/mpws2-config.json') as config_file:
-    config = json.load(config_file)
+file = "mpws2-config.json"
+
+if platform.system().lower() == "windows":
+    win_dir = "C:/Users/Public/appdata"
+    win_path = os.path.join(win_dir, file)
+    with open(win_path) as config_file:
+        config = json.load(config_file)
+else:
+    linux_dir = "/etc/"
+    linux_path = os.path.join(linux_dir, file)
+    with open(linux_path) as config_file:
+        config = json.load(config_file)
 
 
 class Config:
     # Application configurations
-    SECRET_KEY = config.get('SECRET_KEY')
+    SECRET_KEY = config.get("SECRET_KEY")
